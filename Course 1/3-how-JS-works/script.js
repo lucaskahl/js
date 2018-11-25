34a13572b6371db274afba7cec49f917a5897b70
lucaskahl <lucaschangkahl@gmail.com>
2018-11-25 21:00:15 -0200

    Coding and showing what is Hoisting, and JS Scope

--- Course 1/3-how-JS-works/script.js
+++ Course 1/3-how-JS-works/script.js
@@ -1,25 +1,41 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1999); // Ainda funciona (Hoisting), o JS Lê todo o arquivo e você ainda consegue chama-lo antes de declara-lo, só funciona para function declaration
 
+// Funções
+// function declaration
+function calculateAge(year) {
+	console.log(2018 - year);
+}
 
+// function expression
+// retorna erro se chama-lo antes de declara-lo
 
+//retirement(1999); // ERRO, SÓ FUNCIONA DEPOIS DE DECLARA-LO
 
+var retirement = function(year) {
+	console.log(65 - (2016 - year));
+};
 
+retirement(1999); // OK FUNCIONA
 
+// Variaveis
 
+//console.log(age); // undefined
+var age = 23;
+//console.log(age); // 23
 
-
-
-
-
-
-
-
+function foo() {
+	console.log(age); // undef.
+	var age = 65;
+	console.log(age); // 65
+}
+foo();
+console.log(age); // 65;
 
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -37,8 +53,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +76,5 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
-
-
-
-
-
-
-
-
-
